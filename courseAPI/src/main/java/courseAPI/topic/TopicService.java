08bf5387e2e861e39e4ef7fce22bee14e4851c1f
Mahfuz Uddin <mahfuzuddin321@gmail.com>
2022-08-31 16:00:56 -0400

    learning boot spring rest api

---
+++ courseAPI/src/main/java/courseAPI/topic/TopicService.java
@@ -0,0 +1,30 @@
+package courseAPI.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = Arrays.asList(
+			new Topic("spring", "Spring FrameWork", "Spring Framework desc"),
+			new Topic("java", "Core Java", "Core Java desc"),
+			new Topic("javascript", "Javascript", "javascript desc")
+			);
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	
+}
+
+
+
+
